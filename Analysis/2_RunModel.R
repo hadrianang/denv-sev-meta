bfa47526bf130d7b1effa58889543e0fdadf004b
Hadrian Ang <hadrianang@Hadrians-Mac-mini.local>
2026-04-01 15:33:00 +0100

    Created the fit_model function to make multiple fits easier

--- Analysis/2_RunModel.R
+++ Analysis/2_RunModel.R
@@ -20,30 +20,38 @@ if (!dir.exists(output_dir)) {
 }
 
 #We re-run this code three times, once for each fo the severity classification systems listed below
+sev_classes = c("1997type", "2009type", "hospitalisation")
+model_names = c(_LogisticRegressionModel", "FE_Model", "NoCorr_Model")
 
-#sev_class_type = "1997type"
-#sev_class_type = "2009type"
-sev_class_type = "hospitalisation"
 
-data_filename = paste0("data_", sev_class_type, ".rds")
 
-input_data_path = file.path(model_input_dir, data_filename)
-input_data = readRDS(input_data_path)
-data_list = input_data$data_list
+fit_model = function(sev_class_type, model_name, data_suffix = "", iter = 10000, chains = 4, max_treedepth = 12, adapt_delta = 0.99) {
+  #Read the data based on some inputted parameters
+  data_filename = paste0("data_", sev_class_type, data_suffix, ".rds")
+  input_data_path = file.path(model_input_dir, data_filename)
+  input_data = readRDS(input_data_path)
 
-#Some parameters for the priors
-data_list$coeff_prior_mean = 0
-data_list$coeff_prior_sd = 2
+  #Some other parameters added to data_list (currently hardcoded but could be made more flexible in the future)
+  data_list = input_data$data_list
+  data_list$coeff_prior_mean = 0
+  data_list$coeff_prior_sd = 2
 
-data_list$sd_prior_mean = 0.5
-data_list$sd_prior_sd = 2
+  data_list$sd_prior_mean = 0.5
+  data_list$sd_prior_sd = 2
 
-data_list$n_tau_sim = 1000 
+  data_list$n_tau_sim = 1000
 
+  #Assume that the model code is in the model_code_dir and we just load based on the model_name input
+  model = stan_model(file.path(model_code_dir, paste0(model_name, ".stan")))
+  fit_obj = sampling(model, data = data_list, iter = iter, chains = chains, seed = 0,
+                     control = list(max_treedepth = max_treedepth, adapt_delta = adapt_delta))
+
+  return(fit_obj)
+}
 #model = stan_model(file.path(model_code_dir, "LogisticRegressionModel.stan"))
 #model = stan_model(file.path(model_code_dir, "FE_Model.stan"))
 
-model = stan_model(file.path(model_code_dir, "NoCorr_Model.stan"))
+#model = stan_model(file.path(model_code_dir, "NoCorr_Model.stan"))
 # fit_model = sampling(model, data = data_list, iter = 10000, chains = 4, seed = 0,
 #                      control = list(max_treedepth = 12, adapt_delta = 0.99))
 
